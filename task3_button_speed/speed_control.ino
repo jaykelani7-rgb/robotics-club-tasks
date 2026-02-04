235c9cf906527b242c29c0b6e165bd479b737437
Jay Kelani <jaykelani7@gmail.com>
2026-02-04 19:51:51 +0530

    Changed LED pin from 9 to 13

--- task3_button_speed/speed_control.ino
+++ task3_button_speed/speed_control.ino
@@ -1,13 +1,28 @@
-int ledPin = 9;
-int delayTime = 600;
+int ledPin = 13;
+int buttonPin = 2;
+
+int speeds[] = {200, 500, 1000};
+int index = 0;
+
+bool lastButtonState = HIGH;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
+  bool buttonState = digitalRead(buttonPin);
+
+  if (lastButtonState == HIGH && buttonState == LOW) {
+    index++;
+    if (index > 2) index = 0;
+  }
+
+  lastButtonState = buttonState;
+
   digitalWrite(ledPin, HIGH);
-  delay(delayTime);
+  delay(speeds[index]);
   digitalWrite(ledPin, LOW);
-  delay(delayTime);
+  delay(speeds[index]);
 }
